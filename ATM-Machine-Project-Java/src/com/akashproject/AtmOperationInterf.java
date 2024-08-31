ca2162c0a3f3b050d967bbc4a9618206119c248f
Akash Patil <135011385+Akash735077@users.noreply.github.com>
2024-08-31 14:00:43 +0530

    Add files via upload

---
+++ ATM-Machine-Project-Java/src/com/akashproject/AtmOperationInterf.java
@@ -0,0 +1,8 @@
+package com.akashproject;
+
+public interface AtmOperationInterf {
+    public void viewBalance();
+    public void withdrawAmount(double withdrawAmount);
+    public void depositAmount(double depositAmount );
+    public  void viewMiniStatement();
+}
